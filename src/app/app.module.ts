f56eefdf6507e5569ac5bc9ffa6cb912ddc29b93
jml0128 <361029007@qq.com>
2017-09-08 18:38:39 +0800

    通过路由功能实现分类页面与首页的切换，以及不同分类展现不同的内容

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -7,6 +10,14 @@ import { FooterComponent } from './footer/footer.component';
 import { BannerComponent } from './banner/banner.component';
 import { ProductComponent } from './product/product.component';
 import { NewsComponent } from './news/news.component';
+import { CategoryComponent } from './category/category.component';
+import { NewsBannerComponent } from './news-banner/news-banner.component';
+
+const routes: Routes = [
+  {path: '',component: NewsBannerComponent},
+  {path: 'category/:id',component: CategoryComponent},
+  {path: 'category',component: CategoryComponent}
+]
 
 @NgModule({
   declarations: [
@@ -15,10 +26,14 @@ import { NewsComponent } from './news/news.component';
     FooterComponent,
     BannerComponent,
     ProductComponent,
-    NewsComponent
+    NewsComponent,
+    CategoryComponent,
+    NewsBannerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	RouterModule.forRoot(routes),
+	HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+
 
 @Component({
   selector: 'app-navbar',
@@ -6,28 +9,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  //声明一个装载从接口获取的数据的变量
+  private categoryJson: any;
+  //声明分类
   private Products : Array<Product>;
+  //声明一个分类id字段
+  private categoryId : number;
   
-  constructor() {}
-
+  constructor(
+	private routeInfo: ActivatedRoute,
+	private router: Router,
+	private http: HttpClient
+  ){}
+  
   ngOnInit() {
+  
+  
     this.Products = [
-	  new Product(1,"推荐",1),
-	  new Product(2,"居家",0),
-	  new Product(3,"玩家",0),
-	  new Product(4,"吃货",0),
-	  new Product(5,"情趣",0),
-	  new Product(6,"穿搭",0),
-	  new Product(7,"玩具",0),
-	  new Product(8,"婴童",0),
+	  new Product(1,"推荐",1,0),
+	  new Product(2,"居家",0,1),
+	  new Product(3,"玩家",0,2),
+	  new Product(4,"吃货",0,3),
+	  new Product(5,"情趣",0,4),
+	  new Product(6,"穿搭",0,5),
+	  new Product(7,"玩具",0,6),
+	  new Product(8,"婴童",0,7),
 	];
+	
+	this.categoryId = this.routeInfo.snapshot.params["id"]
   }
-
+  
+  toGoodsCategory(id,url){
+    if(url == 0){
+	  this.router.navigate(['./']);
+	}else{
+	  this.router.navigate(['/category',url]);
+	}
+	for(var i in this.Products){
+	  this.Products[i].status = 0;
+	}
+	this.Products[id-1].status = 1;
+  }
+  
 }
+
   export class Product{
     constructor(
 	  public  id:number,
 	  public  name:string,
-	  public  status:number
+	  public  status:number,
+	  public  url:number
 	){}
   }
+  
